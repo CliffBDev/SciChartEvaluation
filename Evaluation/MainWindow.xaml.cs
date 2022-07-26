f5202c8cf938ea8fd5a364e96eab5974313b2674
cbateman <cbateman@SuperSystemsInc.onmicrosoft.com>
2022-07-26 15:18:53 -0400

    Added additional tests.

--- Evaluation/MainWindow.xaml.cs
+++ Evaluation/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,10 +35,71 @@ namespace Evaluation
 
         private void Print(object sender, RoutedEventArgs e)
         {
-            var chartFile = $"{AppDomain.CurrentDomain.BaseDirectory}chart.jpg";
+            Example1();
+        }
+
+        void Example1()
+        {
+            var chartFile = $"{AppDomain.CurrentDomain.BaseDirectory}chart1.jpg";
             bool useXamlRenderSurface = false;
             Size exportedSize = new Size(600, 400);
             Surface.ExportToFile(chartFile, ExportType.Jpeg, useXamlRenderSurface, exportedSize);
+            Process.Start(chartFile);
+        }
+
+        void Example2()
+        {
+            BitmapSource bitmap = Surface.ExportToBitmapSource();
+            var chartFile = $"{AppDomain.CurrentDomain.BaseDirectory}chart2.jpg";
+            SaveImageToFile(chartFile, bitmap);
+            Process.Start(chartFile);
+        }
+
+        void Example3()
+        {
+            var chartFile = $"{AppDomain.CurrentDomain.BaseDirectory}chart3.jpg";
+            bool useXamlRenderSurface = false;
+            Size? exportedSize = null;
+            var bitmapSource = Surface.ExportToBitmapSource(useXamlRenderSurface, exportedSize);
+            SaveImageToFile(chartFile, bitmapSource);
+            Process.Start(chartFile);
+        }
+
+        private void Print2(object sender, RoutedEventArgs e)
+        {
+            Example2();
+        }
+
+        private void Print3(object sender, RoutedEventArgs e)
+        {
+            Example3();
+        }
+
+        public void SaveImageToFile(string filePath, BitmapSource source)
+        {
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                BitmapEncoder encoder = new JpegBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(source));
+                encoder.Save(fileStream);
+            }
+        }
+
+        private void PrintUIEl(object sender, RoutedEventArgs e)
+        {
+            var chartFile = $"{AppDomain.CurrentDomain.BaseDirectory}chart4.jpg";
+
+            RenderTargetBitmap rtb = new RenderTargetBitmap((int)Surface.ActualWidth, (int)Surface.ActualHeight, 96, 96, System.Windows.Media.PixelFormats.Default);
+            rtb.Render(Surface);
+  
+            JpegBitmapEncoder jpgEncoder = new JpegBitmapEncoder();
+            jpgEncoder.Frames.Add(BitmapFrame.Create(rtb));
+
+            using (var fs = System.IO.File.OpenWrite(chartFile))
+            {
+                jpgEncoder.Save(fs);
+            }
+
             Process.Start(chartFile);
         }
     }
